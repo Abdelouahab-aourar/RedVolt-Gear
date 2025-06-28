4f0571dd28f04dacbe133b07d4e4b70d0a83d168
Abdelouahab_a <wahabaourar00@gmail.com>
2025-06-28 18:24:01 +0100

    edit home section

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,7 +6,10 @@ function Home(){
     <div className={styles.home}>
         <div className={styles.left}>
             <h1>RedVolt Gear<br/> Sell Pc Accessories</h1>
-            <p>Welcome to RedVolt Gar Ecommerce website, in which we provide the highest quality of accessories that you need with the best price the market.</p>
+            <p>Welcome to RedVolt Gear — your one-stop shop for premium laptop accessories at unbeatable prices.
+            Whether you're a student, professional, gamer, or tech enthusiast, we’ve got the gear to keep your setup efficient, stylish, and protected.</p>
+            <p>Explore our collection and experience top-tier products backed by reliable customer service, fast shipping, and exclusive deals you won’t find anywhere else.
+            <span> Upgrade your gear. Empower your workspace. Only at RedVolt.</span></p>
             <div className={styles.buttons}>
                 <button>Login</button>
                 <button>Sign Up</button>
