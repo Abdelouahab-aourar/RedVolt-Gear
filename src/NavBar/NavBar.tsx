a31c102fc172348aeac9d7a475c0b7c3c998aab2
Abdelouahab_a <wahabaourar00@gmail.com>
2025-06-28 14:57:55 +0100

    navbar css adds

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,13 +1,18 @@
 import styles from './NavBar.module.css'
+import logo from '../assets/icon.svg'
 function NavBar(){
  return (
     <>
     <nav className={styles.nav}>
       <div className={styles.left}>
-         
+         <img src={logo} alt="logo"/>
+         <p>RedVolt Gear</p>
       </div>
       <div className={styles.right}>
-         
+         <p>HOME</p>
+         <p>PRODUCTS</p>
+         <p>MY CART</p>
+         <p>CONTACT US</p>
       </div>
     </nav>
     </>
