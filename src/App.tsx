64fa67c9c5770ee7612632b5f663e8e84a356b7d
Abdelouahab_a <wahabaourar00@gmail.com>
2025-06-28 12:31:47 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './NavBar/NavBar.tsx'
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import styles from './NavBar.module.css'
+function NavBar(){
+ return (
+    <>
+    <nav className={styles.nav}>
+      <div className={styles.left}>
+         
+      </div>
+      <div className={styles.right}>
+         
+      </div>
+    </nav>
+    </>
+ );   
+}
+export default NavBar;
