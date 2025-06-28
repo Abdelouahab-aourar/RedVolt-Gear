19ae68d8b8e22500527019a049f65e4c9719b692
Abdelouahab_a <wahabaourar00@gmail.com>
2025-06-28 19:25:35 +0100

    some modification

---
+++ src/App/App.tsx
@@ -0,0 +1,22 @@
+import NavBar from '../NavBar/NavBar.tsx'
+import Home from '../Home/Home.tsx'
+import Products from '../Products/Products.tsx'
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <Home/>
+    <Products/>
+    <Products/>
+    <Products/>
+    <Products/>
+    <Products/>
+    <Products/>
+    </>
+  )
+}
+
+export default App
--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -2,6 +2,14 @@ import styles from './Products.module.css'
 function Products(){
     return (
         <>
+            <div className={styles.card}>
+                <div className={styles.top}>
+                    title and image
+                </div>
+                <div className={styles.bottom}>
+                    buttons
+                </div>
+            </div>
         </>
     );
 }
