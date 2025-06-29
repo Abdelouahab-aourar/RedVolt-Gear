95184b62c11bd80a8eab8a16876dd5227f03acdc
Abdelouahab_a <wahabaourar00@gmail.com>
2025-06-29 22:28:00 +0100

    creating slider

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,14 @@
 import NavBar from '../NavBar/NavBar.tsx'
 import Home from '../Home/Home.tsx'
-import Products from '../Products/Products.tsx'
+// import Products from '../Products/Products.tsx'
 import styles from './App.module.css'
-import airbuds from '../assets/airbuds.png'
-import hdd from '../assets/hdd.png'
-import headset from '../assets/headset.png'
-import keyboard from '../assets/keyboard.png'
-import mouse from '../assets/mouse.png'
-import smartWatch from '../assets/smart-watch.png'
+import Slides from '../Slides/Slides.tsx'
+// import airbuds from '../assets/airbuds.png'
+// import hdd from '../assets/hdd.png'
+// import headset from '../assets/headset.png'
+// import keyboard from '../assets/keyboard.png'
+// import mouse from '../assets/mouse.png'
+// import smartWatch from '../assets/smart-watch.png'
 function App() {
 
   return (
@@ -15,14 +16,16 @@ function App() {
     <NavBar/>
     <Home/>
     <h1 className={styles.OurProducts}>Our Products</h1>
-    <div className={styles.productsContainer}>
+    {/* <div className={styles.productsContainer}>
       <Products title={"Wireless Earbuds"} image = {airbuds}/>
       <Products title={"1TB External Hard Drive"} image = {hdd}/>
       <Products title={"Wireless Gaming Headset"} image = {headset}/>
       <Products title={"Wired Gaming Keyboard"} image = {keyboard}/>
       <Products title={"Razer Basilisk Gaming Mouse"} image = {mouse}/>
       <Products title={"Smart Watch"} image = {smartWatch}/>
-    </div>
+    </div> */}
+    <Slides/>
+
     </>
   )
 }
---
+++ src/Slides/Slides.tsx
@@ -0,0 +1,47 @@
+import airbuds from '../assets/airbuds.png'
+import hdd from '../assets/hdd.png'
+import headset from '../assets/headset.png'
+import keyboard from '../assets/keyboard.png'
+import mouse from '../assets/mouse.png'
+import smartWatch from '../assets/smart-watch.png'
+import Products from '../Products/Products'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+function Slides(){
+    return(
+        <div className='wahab'>
+         <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={30}
+        slidesPerView={2}
+        navigation
+        pagination={{ clickable: true }}
+        loop
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <Products title="Wireless Earbuds" image={airbuds} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Products title="1TB External Hard Drive" image={hdd} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Products title="Wireless Gaming Headset" image={headset} />
+        </SwiperSlide><SwiperSlide>
+          <Products title="Wired Gaming Keyboard" image = {keyboard}/>
+        </SwiperSlide><SwiperSlide>
+          <Products title="Razer Basilisk Gaming Mouse" image = {mouse}/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Products title="Smart Watch" image = {smartWatch}/>
+        </SwiperSlide>
+      </Swiper>
+        </div>
+    );
+}
+export default Slides;
