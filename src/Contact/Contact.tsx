7e06ce64c86dab1de23b2a71a224ef75ed78f268
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-01 16:38:25 +0100

    add newsletter section

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,11 +1,26 @@
 import styles from './Contact.module.css'
+import icon from '../assets/icon.svg'
 function Contact(){
     return (
         <>
         <h1 className={styles.title}>Contact Us</h1>
         <div className={styles.container}>
             <div className={styles.top}>
-                info
+                <div className={styles.newsletter}>
+                    <img src={icon} alt="logo" />
+                    <p>Join our newsletter to stay up to date on our new products.</p>
+                    <div className={styles.buttons}>
+                        <input type="email"/>
+                        <button>Subscribe</button>
+                    </div>
+                    <p>By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our company.</p>
+                </div>
+                <div className={styles.contactUs}>
+
+                </div>
+                <div className={styles.followUs}>
+
+                </div>
             </div>
             <div className={styles.bottom}>
                 <div className={styles.line}></div>
